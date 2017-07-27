578de02a7d594dc344bde250e47664934013813a
Mohd Danish Khan <danish0hack@gmail.com>
2017-07-27 13:47:10 +0530

    Initial Commit

---
+++ MLCoreImageRecognization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLCoreImageRecognization
+//
+//  Created by Danish Khan on 27/07/17.
+//  Copyright © 2017 Danish Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
